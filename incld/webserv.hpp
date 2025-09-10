2564d9aa1614f9b6c149c0c276174e5bebde33b4
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-09-10 15:47:36 +0300

    feat: create simple server

--- incld/webserv.hpp
+++ incld/webserv.hpp
@@ -6,8 +6,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #include "CustomException.hpp"
 
 #define IN_PORT "8080"
 #define IN_DOMAIN "localhost"
+#define QUEUE_SIZE 20

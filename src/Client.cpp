2a2a31ff8bfc32b96dcb99d85602ff87ae9a1dbd
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-10-03 14:54:11 +0300

    fix: del obselete data in Client, fix bugs

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,17 +1,22 @@
 #include "Client.hpp"
 
-void	Client::sendResponse(epoll_event &ev, int epollFd, int eventFd)
+void	Client::sendFile(epoll_event &ev, int epollFd, int eventFd)
 {
-	int flags = MSG_NOSIGNAL;
+	std::cout << "Sending file..." << std::endl;
+	int flags = MSG_NOSIGNAL | MSG_MORE;
 
-	int err = send(_clientFd, _fileBuffer.c_str(), _fileBuffer.size(), flags);
-	if (err)
+	if (!_fileBuffer.empty())
 	{
-		THROW_ERRNO("send");
+		int err = send(_clientFd, _fileBuffer.c_str(), _fileBuffer.size(), flags);
+		if (err < 0)
+		{
+			THROW_ERRNO("send");
+		}
 	}
 
 	if (_fileSize == _readFileBytes)
 	{
+		closeFile(ev, epollFd, eventFd);
 		_state = clientState::READING_CLIENT_HEADER;
 		auto it = _handlersMap.find(_clientFd);
 		if (it == _handlersMap.end())
@@ -20,22 +25,39 @@ void	Client::sendResponse(epoll_event &ev, int epollFd, int eventFd)
 		}
 		(*it).second = &_ipPort;
 	}
+	std::cout << "Sending file is done" << std::endl;
 }
 
-void	Client::closeFileDelEpoll(epoll_event &ev, int epollFd, int eventFd)
+void	Client::sendResponse(epoll_event &ev, int epollFd, int eventFd)
 {
-	epoll_ctl(epollFd, EPOLL_CTL_DEL, _fileFd, 0);
-	_handlersMap.erase(_fileFd);
+	int flags = MSG_NOSIGNAL | MSG_MORE;
 
+	std::ostringstream	oss;
+	oss << "HTTP/1.1 200 OK\r\n"
+		<< "Content-Length: " << _fileSize << "\r\n"
+		<< "Content-Type: text/html\r\n"  // or detect MIME type
+		<< "\r\n";
+	std::string header = oss.str();
 
+	int err = send(_clientFd, header.c_str(), header.size(), flags);
+	if (err < 0)
+	{
+		THROW_ERRNO("send");
+	}
+	_state = clientState::READING_FILE;
+}
+
+void	Client::closeFile(epoll_event &ev, int epollFd, int eventFd)
+{
 	close(_fileFd);
 	_fileBuffer.clear();
+	_fileSize = 0;
+	_readFileBytes = 0;
 }
 
-void	Client::openFileAddEpoll(epoll_event &ev, int epollFd, int eventFd)
+void	Client::openFile(epoll_event &ev, int epollFd, int eventFd)
 {
 	int			err;
-	epoll_event	newEv;
 	struct stat	fileInfo;
 
 	_fileFd = open(_filePath.c_str(), O_RDWR, 667);
@@ -50,16 +72,14 @@ void	Client::openFileAddEpoll(epoll_event &ev, int epollFd, int eventFd)
 	_fileSize = fileInfo.st_size;
 	_readFileBytes = 0;
 
-	newEv.data.fd = _fileFd;
-	newEv.events = EPOLLIN | EPOLLOUT;
 
-	_handlersMap.emplace(_fileFd, this);
-	newEv.events = EPOLLIN | EPOLLOUT;
-	newEv.data.fd = _fileFd;
-	err = epoll_ctl(epollFd, EPOLL_CTL_ADD, _fileFd, &newEv);
-	if (err)
-		THROW_ERRNO("epoll_ctl");
-	_state = clientState::READING_FILE;
+	_state = clientState::SENDING_RESPONSE;
+	auto it = _handlersMap.find(_clientFd);
+	if (it == _handlersMap.end())
+	{
+		THROW_ERRNO("Unknown fd in map");
+	}
+	(*it).second = this;
 }
 
 void	Client::readFile(epoll_event &ev, int epollFd, int eventFd)
@@ -72,55 +92,48 @@ void	Client::readFile(epoll_event &ev, int epollFd, int eventFd)
 	if (readBytes > 0)
 	{
 		read_buf[readBytes] = 0;
-		_fileBuffer += read_buf;
+		_fileBuffer = read_buf;
 		_readFileBytes += readBytes;
 
 		std::cout << "FILE BUFFER:\n" << _fileBuffer << std::endl;
-		_state = clientState::SENDING_RESPONSE;
+		_state = clientState::SENDING_FILE;
 	}
 	else if (readBytes == 0)
 	{
+		std::cout << "file size: " << _fileSize << ", readByes" << _readFileBytes << std::endl;
 		THROW_ERRNO("read");
 	}
 	else if (readBytes < 0)
 	{
 		THROW_ERRNO("read");
 	}
-
-	if (_fileSize == _readFileBytes)
-	{
-		closeFileDelEpoll(ev, epollFd, eventFd);
-		_state = clientState::SENDING_RESPONSE;
-	}
 }
 
 void	Client::handleEpollEvent(epoll_event &ev, int epollFd, int eventFd)
 {
-	if (ev.events == EPOLLIN)
+	if (ev.events & EPOLLIN)
 	{
-		if (_state == clientState::READING_FILE)
+		if (_state == clientState::GETTING_FILE)
 		{
-			if (eventFd == _fileFd)
-			{
-				readFile(ev, epollFd, eventFd);
-			}
+
 		}
 	}
-	else
+	else if (ev.events & (EPOLLOUT | EPOLLHUP))
 	{
 		if (_state == clientState::SENDING_RESPONSE)
 		{
-			if (eventFd == _clientFd)
-			{
-				sendResponse(ev, epollFd, eventFd);
-			}
+			sendResponse(ev, epollFd, eventFd);
+		}
+
+		if (_state == clientState::SENDING_FILE)
+		{
+			sendFile(ev, epollFd, eventFd);
+		}
+		if (_state == clientState::READING_FILE)
+		{
+			readFile(ev, epollFd, eventFd);
 		}
 	}
-}
-
-void	Client::setEpollInfo(IEpollInfo *epollInfo)
-{
-	_epollInfo = epollInfo;
 }
 
 // Getters + Setters
@@ -146,7 +159,6 @@ Client::Client(sockaddr_storage clientAddr, socklen_t	clientAddrLen, int	clientF
 	, _fileBuffer()
 	, _fileSize{0}
 	, _readFileBytes{0}
-	, _epollInfo{nullptr}
 {}
 
 Client::~Client()

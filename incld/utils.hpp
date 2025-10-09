9e9a93544c3fb2a3cb6bf9818e44f207eae0216d
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-10-09 18:15:53 +0300

    feat(utils): add changeEpollHandler()

--- incld/utils.hpp
+++ incld/utils.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <fcntl.h>
+#include "webserv.hpp"
 
 namespace utils
 {
@@ -8,7 +8,18 @@ namespace utils
 inline void	makeFdNonBlocking(int fd)
 {
 	int	flags = fcntl(fd, F_GETFL, 0);
-	fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+	if (flags == -1)
+		THROW_ERRNO("fcntl(F_GETFL)");
+	if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
+		THROW_ERRNO("fcntl(F_SETFL)");
+}
+
+inline void	changeEpollHandler(FdEpollOwnerMap &map, int fd, IEpollFdOwner *newHandler)
+{
+	auto elem = map.find(fd);
+	if (elem == map.end())
+		THROW("Uknown fd in FdEpollOwnerMap");
+	elem->second = newHandler;
 }
 
 }

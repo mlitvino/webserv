f046c08088508fc8e66ab2009a7e68b04c1f0813
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-10-14 17:33:39 +0300

    feat(IpPort): create PostRequestHandler class

---
+++ incld/PostRequestHandler.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "webserv.hpp"
+#include "Client.hpp"
+
+class IpPort;
+
+enum class BodyReadStatus
+{
+	NEED_MORE,
+	COMPLETE,
+	ERROR
+};
+
+class PostRequestHandler
+{
+	private:
+		IpPort &_ipPort;
+
+		BodyReadStatus	getContentLengthBody(ClientPtr &client);
+		BodyReadStatus	getChunkedBody(ClientPtr &client);
+		bool			getMultiPart(ClientPtr &client);
+
+		bool			extractFilename(ClientPtr &client, std::string &dashBoundary);
+		std::string		composeUploadPath(ClientPtr &client);
+		void			writeBodyPart(ClientPtr &client, std::string &uploadPath, size_t tailSize);
+		void			getLastBoundary(ClientPtr &client, std::string &boundaryMarker);
+	public:
+		explicit		PostRequestHandler(IpPort &owner);
+
+		void			handlePostRequest(ClientPtr &client, const std::string &path);
+
+
+};
